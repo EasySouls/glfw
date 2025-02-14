9fedb0d1348ff2e4d5f090b91ff3d10094fe009d
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-02-14 16:19:46 +0100

    Update build configurations in premake5.lua
    Comment out warnings, adjust runtime options for Debug and Release configurations, and add sanitization flags for Debug-AS. Retain symbols "off" for Dist configuration to disable debug symbols in the final build.

--- premake5.lua
+++ premake5.lua
@@ -2,7 +2,7 @@ project "GLFW"
 	kind "StaticLib"
 	language "C"
 	staticruntime "off"
-	warnings "off"
+	--warnings "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -100,19 +100,23 @@ project "GLFW"
 
 	filter "configurations:Debug"
 		runtime "Debug"
+		buildoptions "/MDd"
 		symbols "on"
 
 	filter { "system:windows", "configurations:Debug-AS" }	
 		runtime "Debug"
+		buildoptions "/MDd"
 		symbols "on"
 		sanitize { "Address" }
 		flags { "NoRuntimeChecks", "NoIncrementalLink" }
 
 	filter "configurations:Release"
 		runtime "Release"
+		buildoptions "/MD"
 		optimize "speed"
 
     filter "configurations:Dist"
 		runtime "Release"
+		buildoptions "/MD"
 		optimize "speed"
         symbols "off"
